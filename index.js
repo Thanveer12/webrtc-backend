362574da176f0fb57666841dd778a3964dda86d8
Thanveer Ahamed A <thanveerahamed1225@gmail.com>
2023-06-27 11:34:05 +0530

    push

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-// Import packages
-const express = require("express");
-const home = require("./routes/home");
-
-// Middlewares
+const express = require('express');
+const serverless = require('serverless-http');
 const app = express();
-app.use(express.json());
+const router = express.Router();
 
-// Routes
-app.use("/home", home);
+let records = [];
 
-// connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
-module.exports = app;
+//Get all students
+router.get('/', (req, res) => {
+  res.send('App is running..');
+});
+
+//Create new record
+router.post('/add', (req, res) => {
+  res.send('New record added.');
+});
+
+//delete existing record
+router.delete('/', (req, res) => {
+  res.send('Deleted existing record');
+});
+
+//updating existing record
+router.put('/', (req, res) => {
+  res.send('Updating existing record');
+});
+
+//showing demo records
+router.get('/demo', (req, res) => {
+  res.json([
+    {
+      id: '001',
+      name: 'Smith',
+      email: 'smith@gmail.com',
+    },
+    {
+      id: '002',
+      name: 'Sam',
+      email: 'sam@gmail.com',
+    },
+    {
+      id: '003',
+      name: 'lily',
+      email: 'lily@gmail.com',
+    },
+  ]);
+});
+
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app);
