bf808c4f62d1ff4d535b4bfb8200b74a5a74b8cc
Thanveer Ahamed A <thanveerahamed1225@gmail.com>
2023-08-17 12:25:25 +0530

    changes

--- app.js
+++ app.js
@@ -437,8 +437,10 @@ const createWebRtcTransport = async (router) => {
       const webRtcTransport_options = {
         listenIps: [
           {
-            ip: '100.20.92.101',
-           // ip: '127.0.0.1', // replace with relevant IP address
+            // ip: `100.20.92.101
+            // 44.225.181.72
+            // 44.227.217.144`,
+           ip: '127.0.0.1', // replace with relevant IP address
             // announcedIp: '10.0.0.115',
           }
         ],
